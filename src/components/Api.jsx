1f14ad3a467ca0f0c9e12711be76a2ddc5c08257
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2023-03-07 18:40:33 -0600

    Ternary value

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,11 +1,41 @@
 import React, { useState, useEffect } from "react";
+const API = "https://jsonplaceholder.typicode.com/users";
 
 const Api = () => {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  //Aquí vamos a trabar código de JS - Asymc / Await
+  const getDataApi = async () => {
+    try {
+      //Hacemos fetch de la API y se lo asignamos  a una variable
+      const response = await fetch(API);
+      //Convertimos la respuesta a JSON
+      const data = await response.json();
+      //Asignamos la data a nuestro estado
+      setData(data);
+      //Imprimimos la data en consola
+      console.log(data);
+      //Cambiamos el estado de isLoading a false esto nos servirá para un loader
+      setIsLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getDataApi();
+  }, []);
 
   return (
     <>
+      {isLoaded ? (
+        data.map(() => {
+          return <h1>Hola</h1>;
+        })
+      ) : (
+        <h1>Cargando información de la API</h1>
+      )}
       <h1>Componente API</h1>
     </>
   );
